8b87283d95a34ff3b35ebc12102b518b7d8a7221
Pedro Pazello <email@pedropazello.com>
2021-08-23 19:15:38 -0300

    use process.env.PORT to run app on Heroku

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const logger = require('./config/Logger')
 const app = require('./app')
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   logger.log({
     level: 'info',
     message: 'server running on port 3000'
